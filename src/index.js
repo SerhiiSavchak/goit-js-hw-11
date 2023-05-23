d88ce51371085335e5fbbe3f34985f3b3218b2ec
X-RAY <intelekt200012@gmail.com>
2023-05-23 16:18:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,5 +53,13 @@ function onLoadBtnClick() {
 
     gallery.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   });
 }
